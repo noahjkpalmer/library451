bf29409e7c55044511094bf8096db4f9cd59fe9d
Noah Palmer <noahjkpalmer@gmail.com>
2018-12-02 01:05:57 -0800

    Delete MoveToIX.sh

--- MoveToIX.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-scp index.html main.css main.js connectionData.txt npalmer@ix.cs.uoregon.edu:~/public_html/library451/
-
-scp pages/* npalmer@ix.cs.uoregon.edu:~/public_html/library451/pages/
-
-
