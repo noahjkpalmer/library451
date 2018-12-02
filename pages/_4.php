8bfc191240b7975ee9d745a5ce5ec73d1d15f1b4
Noah Palmer <npalmer@uoregon.edu>
2018-12-02 00:26:41 -0800

    centering objects

--- pages/_4.php
+++ pages/_4.php
@@ -20,7 +20,25 @@
 
             <!-- Break Line -->
             <hr>
-            
+
+             <!-- DISPLAY INFORMATION HERE -->
+
+            <!-- Break Line -->
+            <hr>
+
+            <center>
+                <button class="return">Home</button>
+            </center>
         </div>
     </body>
+    <footer>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <script type="text/javascript">
+            $(".return").click(function(){
+                window.location.href = "../index.html";
+            });
+        </script>
+    </footer>
 </html>
